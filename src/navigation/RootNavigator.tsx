cb1974062d986de7b79fdd309f48d180ec387390
ikokywu <rikiridwan949@gmail.com>
2025-06-17 21:04:55 +0700

    feat: add root navigation

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppNavbar from "../components/Layouts/AppNavbar";
 
 const HomePage = React.lazy(() => import("../screens/home"));
 
 const RootNavigator: React.FC = () => {
   return (
     <BrowserRouter>
+      <AppNavbar />
+      
       <Routes>
         <Route
           path="/"
